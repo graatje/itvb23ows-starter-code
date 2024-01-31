fa8ae778dd1f1a700a67bf137ea1ac6ae921433d
kevin <kevin.plstr@gmail.com>
2024-01-31 21:32:41 +0100

    elaborated gamelogic tests

--- app/tests/Gamelogic.php
+++ app/tests/Gamelogic.php
@@ -7,13 +7,27 @@ include 'main/DatabaseHandler.php';
 
 use PHPUnit\Framework\TestCase;
 
-final class Gamelogic extends TestCase
-{
-    public function testValidMoves(): void
-    {
+final class Gamelogic extends TestCase {
+    public function testValidMoves(): void {
+        ob_start();
         
         $dbHandler = new DatabaseHandler();
         $g = new Game($dbHandler);
-        ob_end_flush();
+        
+        $this->assertEquals($g->getPossibleAddPositions(), ['0,0']);
+
+        $g->addPiece('Q', '0,0');
+
+        $this->assertEquals($g->getPossibleAddPositions(), ['0,1', '0,-1','1,0', '-1,0', '-1,1', '1,-1']);
+
+        $g->addPiece('Q', '0,1');
+
+        $this->assertEquals($g->getPossibleAddPositions(), ['0,-1', '-1,0', '1,-1']);
+        
+        $g->addPiece('B', '-1,0');
+
+        $this->assertEquals($g->getPossibleAddPositions(), ['0,2', '1,1', '-1,2']);
+
+        ob_end_clean();
     }
 }
